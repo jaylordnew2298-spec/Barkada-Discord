bc5f3bc8385d83c3ecc97168a55b26a0ce6891db
cutie011020-hash <cutie011020@gmail.com>
2026-05-11 05:21:33 +0800

    Create barkada.js

---
+++ barkada.js
@@ -0,0 +1,46 @@
+require('dotenv').config();
+const fs = require('fs');
+const express = require('express');
+const { Client, GatewayIntentBits, Collection } = require('discord.js');
+
+// 🌐 EXPRESS SERVER (keep alive)
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Barkada Bot is alive!');
+});
+
+app.listen(PORT, () => {
+  console.log(`🌐 Server running on port ${PORT}`);
+});
+
+// 🤖 DISCORD CLIENT
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.commands = new Collection();
+
+// 📂 LOAD COMMANDS
+const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const command = require(`./commands/${file}`);
+  client.commands.set(command.data.name, command);
+}
+
+// 📂 LOAD EVENTS
+const eventFiles = fs.readdirSync('./events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    client.on(event.name, (...args) => event.execute(...args, client));
+  }
+}
+
+// 🔐 LOGIN
+client.login(process.env.TOKEN);
