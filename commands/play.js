3d215f677881409387b31203a246f38d28726797
cutie011020-hash <cutie011020@gmail.com>
2026-05-11 19:55:46 +0800

    Delete commands/play.js

--- commands/play.js
+++
@@ -1,9 +0,0 @@
-const player = require('../music/player');
-
-module.exports = {
-  name: 'play',
-  async execute(message) {
-    const args = message.content.split(' ').slice(1);
-    player.execute(message, args);
-  }
-};
